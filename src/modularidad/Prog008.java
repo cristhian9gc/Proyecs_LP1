ed942d5c79c3d60b77ce42880f36ab4b5a297f61
Cristian Gamboa <cristian9gc@gmail.com>
2020-07-14 01:37:00 -0500

    programas listos

---
+++ src/modularidad/Prog008.java
@@ -0,0 +1,28 @@
+
+package modularidad;
+
+//Autor: Cristian Gamboa Condori
+
+public class Prog008 {
+    //metodos que reciben parametros
+    public static void saludo(String nombre){
+        System.out.println("Bienvenido "+ nombre);
+        System.out.println("Disfruta tu estadia en este lugar");
+    }
+    public static void saludo2(String nombre, String carrera){
+        System.out.println(nombre+" Disfruta tu estadia en la carrera de "+ carrera);
+    }
+    public static void factorial(int num){
+        int fact =1;
+        for(int i=1; i<=num; i++){
+            fact*=i;
+        }
+        System.out.println("Factorial de "+num+" es: "+ fact);
+    }
+    
+    public static void main(String[] args) {
+        saludo("Cristian");
+        saludo2("Pamela Greta", "Ing. Sistemas");
+        factorial(5);
+    }
+}
---
+++ src/modularidad/Prog009.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modularidad;
+
+//Autor: Cristian Gamboa Condori
+
+public class Prog009 {
+     //Metodos que devuelven valores
+    public static String miuniversidad(){
+        String universidad ="Universidad Nacional Tecnologica de Lima sur";
+        return universidad;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(miuniversidad());
+        System.out.println(miuniversidad().toUpperCase());
+    }
+}
